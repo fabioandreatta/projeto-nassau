9acb973a3d4ad7009f5f779cccdb0cc75a62257b
Fabio <fabio@clearest.com.br>
2017-04-16 22:58:49 -0400

    Estrutura basica dos modulos. Ainda falta o pacote do distribuidor.

---
+++ nassau-modules/nassau-nfe-bi/src/main/java/com/nassau/br/modules/nfe/bi/NFeBIApplication.java
@@ -0,0 +1,17 @@
+package com.nassau.br.modules.nfe.bi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.nassau.br.hbase.HBaseTemplate;
+
+@SpringBootApplication
+@ComponentScan({"com.nassau.br"})
+public class NFeBIApplication {
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(NFeBIApplication.class);
+		HBaseTemplate hbase = context.getBean(HBaseTemplate.class);
+	}
+}
